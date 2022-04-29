659f71c5d9385d0bf0960de29e54a10ee2f70f28
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-04-29 17:56:40 -0400

    added course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,14 @@
 class Course < ApplicationRecord
   has_many :holes, :dependent => :delete_all
 
+  validates :hole_count, presence: true, numericality: { in: 1..21 }
+  validates :course_name, presence: true, length: {minimum:2, maximum:30}
+  validates :website_url, presence: true, length: {minimum:6, maximum:30}
+  validates :hour_open, presence: true, numericality: { in: 0..23 }
+  validates :hour_close, presence: true,numericality: { in: 0..23 }
+  validates :street_address, presence: true, length: {minimum:6, maximum:30}
+  validates :address_locality, presence: true
+  validates :address_region, presence: true
+  validates :postal_code, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Character.create(name: "Luke", movie: movies.first)
+
+
+# this seed isn't quite working yet
+# Course.create({:hole_count 18}, {:membership false}, {:phone "8134180636"}, :website_url "mangrove.com", :hour_open 6, :hour_close 6, :driving_range true, :putting_green true, :chipping_area true, :practice_bunker true, :riding_cart true, :push_cart true, :pro_shop true, :club_rental true, :caddie_services false, :lessons true, :locker_room_onsite true, :shower_onsite true, :lodging_onsite false, :clubfitting false, :punched_greens false, :sanded_greens false, :street_address "344 63rd Ave N", :address_locality "St. Petersburg", :address_region "Florida", :postal_code "33704", :created_at Time.now, :updated_at Time.now, :course_name "Mangrove Bay")
