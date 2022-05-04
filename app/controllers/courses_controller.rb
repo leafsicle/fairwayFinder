1cf01c559054dafa1c2ce5cc433c5d4431610c6f
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-04 12:58:14 -0400

    got the par calc displaying on the index page!!

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -67,4 +67,4 @@ class CoursesController < ApplicationController
     def course_params
       params.require(:course).permit(:course_name, :hole_count, :membership, :phone, :website_url, :hour_open, :hour_close, :driving_range, :putting_green, :chipping_area, :practice_bunker, :riding_cart, :push_cart, :pro_shop, :club_rental, :caddie_services, :lessons, :locker_room_onsite, :shower_onsite, :lodging_onsite, :clubfitting, :punched_greens, :sanded_greens, :street_address, :address_locality, :address_region, :postal_code)
     end
-end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :scorecards
-  resources :holes
-  resources :courses
-  resources :users
+  # namespace :admin do
+    resources :scorecards
+    resources :holes
+    resources :courses
+  # end 
+      resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
