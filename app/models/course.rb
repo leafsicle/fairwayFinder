6a4588be611dd35f3bfaaa74142215f9f9b0e42e
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-05 17:31:17 -0400

    Changes to _course and model appear

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,16 +1,20 @@
 class Course < ApplicationRecord
-  has_many :holes, :dependent => :delete_all
-  has_many :scorecards 
+  has_many :holes, :dependent => :destroy
+  has_many :scorecards
 
   validates :hole_count, presence: true, numericality: { in: 1..21 }
-  validates :course_name, presence: true, length: {minimum:2, maximum:30}, uniqueness: true
-  validates :website_url, presence: true, length: {minimum:6, maximum:30}
+  validates :course_name, presence: true, length: { minimum: 2, maximum: 30 }, uniqueness: true
+  validates :website_url, presence: true, length: { minimum: 6, maximum: 80 }
   validates :hour_open, presence: true, numericality: { in: 0..23 }
-  validates :hour_close, presence: true,numericality: { in: 0..23 }
-  validates :street_address, presence: true, length: {minimum:6, maximum:30}
+  validates :hour_close, presence: true, numericality: { in: 0..23 }
+  validates :street_address, presence: true, length: { minimum: 6, maximum: 30 }
   validates :address_locality, presence: true
   validates :address_region, presence: true
   validates :postal_code, presence: true
 
+  def course_par
+    holes.reduce(0) { |sum, hole| sum + hole.par_value }
+  end
 
+  # TODO: Add a "link to score card pdf?rai"
 end
