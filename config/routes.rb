03f4ef14b73b9d1921f974dcbcc828f992d3e6bd
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-09 11:31:34 -0400

    Revert "added tailwind and default config"
    This reverts commit 5fc00742f2420283c5393da449bf4e16de713bb2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
+    root to: "/courses#index"
     resources :holes
     resources :courses
     resources :users
     resources :scorecards
-    root to: "/admin/courses#index"
   end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
