24ee0b78d3f843ed8f4fdfb435fdd11993477466
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-13 16:42:58 -0400

    removing boilerplate

--- client/first-cut/src/App.jsx
+++ client/first-cut/src/App.jsx
@@ -1,27 +1,18 @@
 import './App.css';
 import CourseList from './components/CourseList.jsx';
+import Navbar from './components/Navbar.jsx';
+import axios from 'axios'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ul class="nav">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
+      <header className="App-header ">
+        <Navbar></Navbar>
       </header>
-      {/* <CourseList/>         */}
-      <p>words</p>
+      <main className='container pt-3'>
+        <CourseList/>
+      <p className="bg-primary">words</p>
+      </main>
     </div>
   );
 }
