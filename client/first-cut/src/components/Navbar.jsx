1b37093c231da2e04c9d977447372b17ff65ae5e
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-13 17:57:54 -0400

    altered navbar component to not break

--- client/first-cut/src/components/Navbar.jsx
+++ client/first-cut/src/components/Navbar.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-      <nav class="navbar navbar-expand-lg bg-light">
-          <div class="container-fluid justify-content-end">
-            <a class="navbar-brand brand-name" href="#">FairwayFinder</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid justify-content-end">
+            <a className="navbar-brand brand-name" href="#">FairwayFinder</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Player Info</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Player Info</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Previous Rounds</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Previous Rounds</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Start a round</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Start a round</a>
                 </li>
               </ul>
             </div>
