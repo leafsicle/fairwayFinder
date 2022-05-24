95fabef1050911ec0511ed5833710bca7312c9ce
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-24 11:31:25 -0500

    prep to move scorecards under course

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -2,6 +2,7 @@ class Scorecard < ApplicationRecord
   belongs_to :user
   belongs_to :course
   has_many :hole_performance
+  has_many :holes, through: :course
 
   validates :user, presence: true
   validates :course_id, presence: true
