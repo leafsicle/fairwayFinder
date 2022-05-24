e0b33bfbe94d8ec67b9f0c8e792790dca9f115c0
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-05-24 13:44:41 -0500

    add icon and provide juice to show holes added count

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
     
   end
   namespace :admin do
+    resources :users 
     resources :courses do
-      resources :holes
-    end
-    resources :users do
       resources :scorecards
+      resources :holes
     end
   end 
 
