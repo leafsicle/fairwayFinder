5a4898894c34101c95699c15593fcb403875fd4a
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-10 15:56:51 -0400

    added todo and set static as home for user auth prompt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root to: "static_pages#home"
+  root to: "static_pages#home"
 
   namespace :api do
     get "/courses/index", to: "courses#index"
@@ -12,11 +12,7 @@ Rails.application.routes.draw do
       resources :holes
     end
   end
-
-  root to: "admin/courses#index"
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
   if Rails.env.production?
     CLIENT_HTML = FILE.read(Rails.root.join("public/index.html"))
     get "*path", to: proc { [200, {}, [CLIENT_HTML]] }
