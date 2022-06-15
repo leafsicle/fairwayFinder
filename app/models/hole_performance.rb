4fa81a1243965cd464f1d67a45faa9af01f9ce25
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-15 04:59:46 -0400

    QOL updates

--- app/models/hole_performance.rb
+++ app/models/hole_performance.rb
@@ -3,25 +3,25 @@ class HolePerformance < ApplicationRecord
   has_one :course
   has_one :hole, through: :course
   has_one :user, through: :scorecard
-  
-  validates :website_url, presence: true, length: { minimum: 6, maximum: 80 }
+
   validates :strokes, presence: true, length: { in: 1..12 }
   validates :greenside_bunker_count, presence: true, length: { in: 0..12 }
   validates :fairway_bunker_count, presence: true, length: { in: 0..12 }
   validates :water_penalty_count, presence: true, length: { in: 0..12 }
   validates :out_of_bounds_penalty, presence: true, length: { in: 0..12 }
   validates :penalty_stroke_count, presence: true, length: { in: 0..12 }
+  validates :putts, presence: true, length: { in: 0..12 }
 
   @total_stroke_count = 0
-  
+
   def count_of_strokes
-    @total_stroke_count = :strokes + 
-    :greenside_bunker_count + 
-    :fairway_bunker_count + 
-    :water_penalty_count + 
-    :out_of_bounds_penalty + 
-    :penalty_stroke_count 
+    @total_stroke_count = :strokes +
+                          :greenside_bunker_count +
+                          :fairway_bunker_count +
+                          :water_penalty_count +
+                          :out_of_bounds_penalty +
+                          :penalty_stroke_count
   end
-  
+
   before_save :count_of_strokes
 end
