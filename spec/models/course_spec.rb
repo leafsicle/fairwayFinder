cd5841b2663fdd255142a09f0e6e09d27e48b8df
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-07-13 13:42:56 -0400

    added 2 tests!

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a passing test" do
+    expect(true).to be_truthy
+  end
+  it "should have a par value" do
+    expect(subject.par_value).to be >= 0
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,8 +27,12 @@ RSpec.configure do |config|
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+  # Use color in STDOUT
+  config.color = true
 
-  # rspec-mocks config goes here. You can use an alternate test double
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html,
+                                    # :json, CustomFormatterClass  # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
@@ -44,8 +48,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
